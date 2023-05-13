29fa68291d287c7608756f241a8a7a2766d4347d
Vladimir Mitev <vmitev2001@abv.bg>
2023-05-13 23:32:03 +0300

    Add

---
+++ 08. XML/01. ProductShop/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            // User
+
+            CreateMap<ImportUserDto, User>();
+
+            // Products 
+
+            CreateMap<ImportProductDto, Product>()
+                .ForMember(d => d.BuyerId.HasValue,
+                opt => opt.MapFrom(s => s.BuyerId!.HasValue));
+        }
+    }
+}
---
+++ 08. XML/01. ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using ProductShop.Utilities;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            string inputXml = File.ReadAllText(@"../../../Datasets/products.xml");
+
+            string result = ImportProducts(context, inputXml);
+
+            Console.WriteLine(result);
+        }
+
+        // Problem 01
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            IMapper mapper = InitializeAutoMapper();
+
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportUserDto[] dto = xmlHelper.Deserialize<ImportUserDto[]>(inputXml, "Users");
+
+            ICollection<User> users = new HashSet<User>();
+
+            foreach (var importUserDto in dto)
+            {
+                User user = mapper.Map<User>(importUserDto);
+
+                users.Add(user);
+            }
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+
+        // Problem 02
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            IMapper mapper = InitializeAutoMapper();
+
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportProductDto[] importProductDtos = 
+                xmlHelper.Deserialize<ImportProductDto[]>(inputXml, "Products");
+
+            ICollection<Product> validProducts = new HashSet<Product>();
+
+            foreach (ImportProductDto productDto in importProductDtos)
+            {
+                if (!productDto.BuyerId.HasValue)
+                {
+                    continue;
+                }
+
+                Product product = mapper.Map<Product>(productDto);
+
+                validProducts.Add(product);
+            }
+
+            context.Products.AddRange(validProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {validProducts.Count}";
+        }
+
+        private static IMapper InitializeAutoMapper()
+        {
+            return new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+        }
+    }
+}
